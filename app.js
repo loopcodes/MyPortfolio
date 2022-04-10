d8e88b8e51b4be1d398abc5b7823bed15e293f38
Paul Osuigwe <osuigwepaul.o@gmail.com>
2022-04-10 22:59:06 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,118 @@
+const navbar = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
+const menubtn = document.querySelector('.menu-btn');
+const scrollup = document.querySelector('.scroll-up-btn');
+const html = document.querySelector('.home');
+const typedText = document.querySelector('.typed-text');
+const sliders = document.querySelectorAll('.column');
+const faders = document.querySelectorAll(".fade-in")
+
+
+
+
+window.addEventListener("scroll",() => {
+    if(window.scrollY > 20){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+    if(window.scrollY > 500){
+        scrollup.classList.add("show");
+    }else{
+        scrollup.classList.remove("show");
+    }
+});
+
+menubtn.addEventListener("click",() => {
+    if(menubtn.firstElementChild.classList.contains("fa-bars")){
+        menubtn.firstElementChild.classList.replace("fa-bars", "fa-times");
+    }else{
+        menubtn.firstElementChild.classList.replace("fa-times", "fa-bars");
+    }
+
+    menu.classList.toggle("active");
+});
+
+scrollup.addEventListener("click", () =>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
+var i = 0;
+var txt = "Frontend Developer.";
+var speed = 100;
+var backspeed = 60;
+
+function typed(){
+    document.getElementById("live").innerHTML +=txt.charAt(i);
+    i++;
+    setTimeout(typed,speed,backspeed);
+}
+typed();
+
+
+
+const textArray = ["HTML.", "CSS.", "ANGULAR.", "JAVASCRIPT.", "TYPESCRIPT.", "BOOTSTRAP."];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 1000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+
+function type(){
+    if(charIndex < textArray[textArrayIndex].length){
+        typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type,typingDelay);
+    }else{
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase(){
+    if(charIndex > 0){      
+        typedText.textContent = textArray[textArrayIndex].substring(0,charIndex-1);  
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }else{
+        textArrayIndex++;
+        if(textArrayIndex >= textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+type();
+
+
+document.addEventListener("DOMContentLoader", function(){
+    setTimeout(type, newTextDelay +250);
+});
+
+
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+};
+
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }else{
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, 
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
