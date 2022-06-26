7e1c6ba35c0e34ebba19a401f22bcb1e40145b82
Paul Osuigwe <osuigwepaul.o@gmail.com>
2022-06-26 14:18:21 +0100

    changes

--- app.js
+++ app.js
@@ -130,19 +130,3 @@ faders.forEach(fader => {
 sliders.forEach(slider => {
     appearOnScroll.observe(slider);
 });
-
-function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "osuigwepaul.o@gmail.com",
-        Password: "Hustle001",
-        To: 'osuigwepaul.o@gmail.com',
-        From: document.getElementById("email").value,
-        Subject: document.getElementById("subject").value,
-        Body: "Name: " + document.getElementById("name").value +
-            "<br> Email: " + document.getElementById("email").value +
-            "<br> Message: " + document.getElementById("message").value
-    }).then(
-        message => alert("Message Sent Successfully")
-    );
-}
